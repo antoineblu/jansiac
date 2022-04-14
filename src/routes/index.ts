9b15a6918a3deefa7f6b192f2b63437fe4ee6810
seyti67 <antoblum67@gmail.com>
2022-04-14 21:04:06 +0200

    day 3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import type { Day } from '$lib/components/Day';
 
 export const days: Day[] = [
 	{
-		title: 'Ça continue!',
+		title: 'Ça continue !',
 		text: "Nous y sommes, nous commençons enfin à poser les murs et l'accomplissement commence à se percevoir. Dans la joie, la bonne humeur et l'humour, on porte, on visse et on rigole. Sastisfaits de n'avoir que quelques légers imprévus, nous profitons d'un chantier agréable malgré un vent prononcé qui n'a pas encore posé de problème. Merci à vous qui suivez ce chantier et merci à ce projet qui nous fait vivre de beaux moments humains.",
 		image: 'https://s5.imgcdn.dev/BafeD.jpg'
 	},
@@ -10,6 +10,11 @@ export const days: Day[] = [
 		title: 'Deuxième jour de labeur',
 		text: "Au bout du deuxième jour, les murs sont presque élevés et même une partie du plafond a commencé à surgir. Tout le monde a toujours ses deux mains et 10 doigts ! L'équipe de 16 à 81 ans collaborent bien dans la bonne humeur. Demain, le dernier mur et le plafond nous attendent. L'ajout d'une porte a bien occupé Théo et son apprenti Antoine. Nous construisons un chalet mais aussi des connaissances chez les jeunes générations. La météo est de notre côté avec un soleil radieux qui appelle la crème solaire.",
 		image: 'https://s5.imgcdn.dev/Ba8cM.jpg'
+	},
+	{
+		title: "Un peu d'ombre",
+		text: "Les murs sont tous debouts et le plafond attend sa laine de verre ! Les premières plaques du plafond offrent une ombre bienvenue avec le soleil qui est de plus en plus présent. Pour l'intérieur, nous avons posé le pare-vapeur qui permet une meilleure isolation de la laine de verre dangereuse pour la santé. Le toit pose encore quelques questions mais pour le moment, il s'agirait de rendre le chalet habitable et venir finir la toiture (en la modifiant) pour qu'elle résiste à la météo extrême de Jansiac.",
+		image: 'https://s5.imgcdn.dev/B1DYh.jpg'
 	}
 ];
 
@@ -25,5 +30,11 @@ export const gallery = [
 	{ url: 'https://s5.imgcdn.dev/BaqgS.jpg' },
 	{ url: 'https://s5.imgcdn.dev/Bage0.jpg', title: 'Main à la patte' },
 	{ url: 'https://s5.imgcdn.dev/BaXGe.jpg' },
-	{ url: 'https://s5.imgcdn.dev/Ba8cM.jpg' }
+	{ url: 'https://s5.imgcdn.dev/Ba8cM.jpg' },
+
+	{ url: 'https://s5.imgcdn.dev/Baotl.jpg' },
+	{ url: 'https://s5.imgcdn.dev/B1Y5V.jpg' },
+	{ url: 'https://s5.imgcdn.dev/B1DYh.jpg' },
+	{ url: 'https://s5.imgcdn.dev/B1hOK.jpg' },
+	{ url: 'https://s5.imgcdn.dev/B1jgo.jpg' }
 ];
