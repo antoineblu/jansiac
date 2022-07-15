e55373008adec1b6e19ef152b7fb90ff830740b8
seyti67 <antoblum67@gmail.com>
2022-07-15 23:09:31 +0200

    season 3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,74 +2,16 @@ import type { Day } from '$lib/components/Day';
 
 export const days: Day[] = [
 	{
-		title: 'Ça continue !',
-		text: "Nous y sommes, nous commençons enfin à poser les murs et l'accomplissement commence à se percevoir. Dans la joie, la bonne humeur et l'humour, on porte, on visse et on rigole. Sastisfaits de n'avoir que quelques légers imprévus, nous profitons d'un chantier agréable malgré un vent prononcé qui n'a pas encore posé de problème. Merci à vous qui suivez ce chantier et merci à ce projet qui nous fait vivre de beaux moments humains.",
-		image: 'https://s5.imgcdn.dev/BafeD.jpg'
-	},
-	{
-		title: 'Deuxième jour de labeur',
-		text: "Au bout du deuxième jour, les murs sont presque élevés et même une partie du plafond a commencé à surgir. Tout le monde a toujours ses deux mains et 10 doigts ! L'équipe de 16 à 81 ans collaborent bien dans la bonne humeur. Demain, le dernier mur et le plafond nous attendent. L'ajout d'une porte a bien occupé Théo et son apprenti Antoine. Nous construisons un chalet mais aussi des connaissances chez les jeunes générations. La météo est de notre côté avec un soleil radieux qui appelle la crème solaire.",
-		image: 'https://s5.imgcdn.dev/Ba8cM.jpg'
-	},
-	{
-		title: "Un peu d'ombre",
-		text: "Les murs sont tous debouts et le plafond attend sa laine de verre ! Les premières plaques du plafond offrent une ombre bienvenue avec le soleil qui est de plus en plus présent. Pour l'intérieur, nous avons posé le pare-vapeur qui permet une meilleure isolation de la laine de verre dangereuse pour la santé. Le toit pose encore quelques questions mais pour le moment, il s'agirait de rendre le chalet habitable et venir finir la toiture (en la modifiant) pour qu'elle résiste à la météo extrême de Jansiac.",
-		image: 'https://s5.imgcdn.dev/B1DYh.jpg'
-	},
-	{
-		title: 'La boîte se ferme',
-		text: "Tout est fermé et isolé ! Le temps est toujours de notre côté malgré quelques gouttes et le soleil qui est plus agréable en fin de journée... Pour ce qui est du moral, l'équipe sourit toujours et les liens entre nous se renforcent ! Ce soir, par exemple, une petite équipe de quatre est allée se laver à l'eau froide sortant de la montagne et acheminée par tuyau. Pour le chantier, il ne nous reste plus qu'à faire la toiture et la séparation intérieure avec des cloisons.",
-		image: 'https://s5.imgcdn.dev/B1MSn.jpg'
-	},
-	{
-		title: 'Les détails',
-		text: "On pourrait commencer à croire qu'il ne reste plus grand chose... Détrompez-vous ! Nous avons fait ce qui est gros, les bases, mais notre équipe ne se contentera pas d'un chalet bricolé. On veut que ce chalet tienne, qu'il soit bien isolé, étanche aux rats et confortable. Nous travaillons sur chaque millimètre pour en faire quelque chose de propre. Pour le plan du chalet, nous sommes partis sur une séparation par cloison isolée à la laine de bois avec une porte. Les deux entrées permettent de faire 2 espaces séparés.",
-		image: 'https://s6.imgcdn.dev/B1KmT.jpg'
-	},
-	{
-		title: 'Une maison',
-		text: "Ça y est presque ! Le chalet a des murs, des portes, le tout isolé. La fatigue se sent autant que la satisfaction. Il est maintenant question d'y retourner pendant les grandes vacances d'été pour faire une toiture plus résistante avec un bout de l'équipe. En attendant, le chalet est bien emballé et ne devrait pas souffrir des éventuelles intempéries qui arriverait d'ici à l'été. Il devrait être habitable et pour le temps que nous avons passé dedans, il y fait bon vivre !",
-		image: 'https://s6.imgcdn.dev/B1Zhl.jpg'
+		title: 'Dernier début',
+		text: "(A priori) <br> Nous avons consacré un bon bout de la matinée à remettre le chantier en route, chercher le bois, les outils et tout mettre en place. Un aménagement d'horaires s'impose avec un soleil ardent et un travail sur le toit. Nous en sommes donc au placement des poutres qui supporterons les tôles et la mise en place d'une porte à l'arrière du chalet. La fatigue vient plus vite avec le soleil mais l'ambiance reste la même et nous avons tous le sourire.",
+		image: 'https://s6.imgcdn.dev/BpP5C.jpg'
 	}
 ];
 
 export const gallery = [
-	{ url: 'https://s5.imgcdn.dev/Bae5i.jpg' },
-	{ url: 'https://s5.imgcdn.dev/BamAH.jpg' },
-	{ url: 'https://s5.imgcdn.dev/BabD2.jpg', title: 'Équilibristes' },
-	{ url: 'https://s5.imgcdn.dev/Badzy.jpg' },
-	{ url: 'https://s5.imgcdn.dev/BaWM9.jpg' },
-	{ url: 'https://s5.imgcdn.dev/BafeD.jpg' },
-
-	{ url: 'https://s5.imgcdn.dev/BaHQC.jpg' },
-	{ url: 'https://s5.imgcdn.dev/BaqgS.jpg' },
-	{ url: 'https://s5.imgcdn.dev/Bage0.jpg', title: 'Main à la patte' },
-	{ url: 'https://s5.imgcdn.dev/BaXGe.jpg' },
-	{ url: 'https://s5.imgcdn.dev/Ba8cM.jpg' },
-
-	{ url: 'https://s5.imgcdn.dev/Baotl.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1Y5V.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1DYh.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1hOK.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1jgo.jpg' },
-
-	{ url: 'https://s5.imgcdn.dev/B1ceg.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1MSn.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1CaN.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B14cv.jpg' },
-	{ url: 'https://s5.imgcdn.dev/B1Itq.jpg' },
-
-	{ url: 'https://s6.imgcdn.dev/B15St.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1KmT.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1TuD.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1Ua9.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1lWy.jpg' },
-
-	{ url: 'https://s6.imgcdn.dev/B1GiS.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1SrC.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1xu0.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1rme.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B121M.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1AWd.jpg' },
-	{ url: 'https://s6.imgcdn.dev/B1Zhl.jpg' }
+	{ url: 'https://s6.imgcdn.dev/Bpib8.jpg' },
+	{ url: 'https://s6.imgcdn.dev/Bpszi.jpg' },
+	{ url: 'https://s6.imgcdn.dev/BpyfH.jpg' },
+	{ url: 'https://s6.imgcdn.dev/BpKDS.jpg' },
+	{ url: 'https://s6.imgcdn.dev/BpP5C.jpg' }
 ];
